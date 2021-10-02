704198e337f954922696385782ac0e82f5a18b22
Vito Botta <vito@botta.me>
2021-10-02 17:45:01 +0300

    Add build script

---
+++ bin/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+
+
+IMAGE="vitobotta/hetzner-k3s"
+
+docker build -t ${IMAGE}:v0.4.1 \
+  --platform=linux/amd64 \
+  --cache-from ${IMAGE}:v0.4.0 \
+  --build-arg BUILDKIT_INLINE_CACHE=1 .
