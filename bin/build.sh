f2154e59d7baaaedd16d506a2bc11b294e3b08f7
Vito Botta <vito@botta.me>
2021-11-03 16:58:36 +0200

    Update build script

--- bin/build.sh
+++ bin/build.sh
@@ -6,9 +6,9 @@ set -e
 
 IMAGE="vitobotta/hetzner-k3s"
 
-docker build -t ${IMAGE}:v0.4.3 \
+docker build -t ${IMAGE}:v0.4.4 \
   --platform=linux/amd64 \
-  --cache-from ${IMAGE}:v0.4.2 \
+  --cache-from ${IMAGE}:v0.4.3 \
   --build-arg BUILDKIT_INLINE_CACHE=1 .
 
-docker push vitobotta/hetzner-k3s:v0.4.3
+docker push vitobotta/hetzner-k3s:v0.4.4
