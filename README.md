4b34e746fc9bfc31d8d721f6d2c40ff0dfe2c0f3
Vito Botta <vito@botta.me>
2022-08-12 18:55:49 +0300

    Update build script

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ Alternatively, if you don't want to set up a Ruby runtime but have Docker instal
 docker run --rm -it \
   -v ${PWD}:/cluster \
   -v ${HOME}/.ssh:/tmp/.ssh \
-  vitobotta/hetzner-k3s:v0.5.8 \
+  vitobotta/hetzner-k3s:v0.5.9 \
   create-cluster \
   --config-file /cluster/test.yaml
 ```
--- bin/build.sh
+++ bin/build.sh
@@ -4,9 +4,9 @@ set -e
 
 IMAGE="vitobotta/hetzner-k3s"
 
-docker build -t ${IMAGE}:v0.5.8 \
+docker build -t ${IMAGE}:v0.5.9 \
   --platform=linux/amd64 \
-  --cache-from ${IMAGE}:v0.5.7 \
+  --cache-from ${IMAGE}:v0.5.8 \
   --build-arg BUILDKIT_INLINE_CACHE=1 .
 
-docker push vitobotta/hetzner-k3s:v0.5.8
+docker push vitobotta/hetzner-k3s:v0.5.9
