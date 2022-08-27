b45566ca277c47cf75c137c93c255716bd152b5c
Vito Botta <vito@botta.me>
2022-08-27 20:17:01 +0300

    Add build script

--- bin/build.sh
+++ bin/build.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-IMAGE="vitobotta/hetzner-k3s"
+# IMAGE="vitobotta/hetzner-k3s"
 
-docker build -t ${IMAGE}:v0.5.9 \
-  --platform=linux/amd64 \
-  --cache-from ${IMAGE}:v0.5.8 \
-  --build-arg BUILDKIT_INLINE_CACHE=1 .
+# docker build -t ${IMAGE}:v0.5.9 \
+#   --platform=linux/amd64 \
+#   --cache-from ${IMAGE}:v0.5.8 \
+#   --build-arg BUILDKIT_INLINE_CACHE=1 .
 
-docker push vitobotta/hetzner-k3s:v0.5.9
+# docker push vitobotta/hetzner-k3s:v0.5.9
+
+warble
+
+echo "#!/usr/bin/env java -jar" > dist/hetzner-k3s
+cat dist/hetzner-k3s.jar >> dist/hetzner-k3s
+chmod +x dist/hetzner-k3s
