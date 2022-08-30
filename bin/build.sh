c21df25d26e32369609be00fbf4ebc59f4997d48
Vito Botta <vito@botta.me>
2022-08-30 18:52:44 +0300

    Bump version to 0.6.2

--- bin/build.sh
+++ bin/build.sh
@@ -4,10 +4,10 @@ set -e
 
 IMAGE="vitobotta/hetzner-k3s"
 
-docker build -t ${IMAGE}:v0.6.1 \
+docker build -t ${IMAGE}:v0.6.2 \
   --platform=linux/amd64 \
-  --cache-from ${IMAGE}:v0.6.0 \
+  --cache-from ${IMAGE}:v0.6.1 \
   --build-arg BUILDKIT_INLINE_CACHE=1 .
 
-docker push vitobotta/hetzner-k3s:v0.6.1
+docker push vitobotta/hetzner-k3s:v0.6.2
 
