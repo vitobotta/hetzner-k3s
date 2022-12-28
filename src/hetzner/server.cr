400d21ad9402aca5f306e56fb809c5b6484e70ef
Vito Botta <vito@botta.me>
2022-12-28 20:31:54 +0200

    Wait until private IP is available

--- src/hetzner/server.cr
+++ src/hetzner/server.cr
@@ -1,5 +1,6 @@
 require "json"
 require "./public_net"
+require "./network_interface"
 
 class Hetzner::Server
   include JSON::Serializable
@@ -7,8 +8,18 @@ class Hetzner::Server
   property id : Int32
   property name : String
   getter public_net : PublicNet?
+  getter private_net : Array(Hetzner::NetworkInterface)?
 
-  def ip_address
+  def public_ip_address
     public_net.try(&.ipv4).try(&.ip)
+  end
+
+  def private_ip_address
+    net = private_net
+
+    return unless net
+    return if net.try(&.empty?)
+
+    net[0].ip
   end
 end
