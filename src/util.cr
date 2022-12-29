48a771a325d067cf25d1233c87e06abd0e83ee77
Vito Botta <vito@botta.me>
2022-12-29 23:19:00 +0200

    Deploy CCM

--- src/util.cr
+++ src/util.cr
@@ -1,2 +1,12 @@
  module Util
+  def self.which(command)
+    exts = ENV.fetch("PATHEXT", "").split(";")
+    ENV["PATH"].split(Process::PATH_DELIMITER).each do |path|
+      exts.each do |ext|
+        exe = File.join(path, "#{command}#{ext}")
+        return exe if File.executable?(exe) && !File.directory?(exe)
+      end
+    end
+    nil
+  end
  end
