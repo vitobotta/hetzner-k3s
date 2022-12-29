7142da90af0883c5b3cc2ae853d56ca0dd3c0ae8
Vito Botta <vito@botta.me>
2022-12-29 19:15:05 +0200

    Deploy k3s on worker nodes

--- lib/hetzner/kubernetes/client.rb
+++ lib/hetzner/kubernetes/client.rb
@@ -155,39 +155,6 @@ module Kubernetes
     end
 
 
-
-
-
-
-
-
-
-
-
-
-
-    def worker_install_script(worker)
-      flannel_interface = find_flannel_interface(worker)
-
-      <<~BASH
-        curl -sfL https://get.k3s.io | K3S_TOKEN="#{k3s_token}" INSTALL_K3S_VERSION="#{k3s_version}" K3S_URL=https://#{first_master_private_ip}:6443 INSTALL_K3S_EXEC="agent \
-          --node-name="$(hostname -f)" \
-          --kubelet-arg="cloud-provider=external" \
-          --node-ip=$(hostname -I | awk '{print $2}') \
-          --node-external-ip=$(hostname -I | awk '{print $1}') \
-          --flannel-iface=#{flannel_interface}" sh -
-      BASH
-    end
-
-
-
-
-
-
-
-
-
-
     def mark_nodes(mark_type:)
       check_kubectl
 
--- src/kubernetes/installer.cr
+++ src/kubernetes/installer.cr
@@ -45,6 +45,7 @@ class Kubernetes::Installer
 
     set_up_first_master
     set_up_other_masters
+    set_up_workers
   end
 
   private def set_up_first_master
@@ -82,6 +83,26 @@ class Kubernetes::Installer
     end
   end
 
+  private def set_up_workers
+    channel = Channel(Hetzner::Server).new
+
+    workers.each do |worker|
+      spawn do
+        puts "Deploying k3s to worker #{worker.name}..."
+
+        ssh.run(worker, worker_install_script(worker))
+
+        puts "...k3s has been deployed to worker #{worker.name}."
+
+        channel.send(worker)
+      end
+    end
+
+    workers.size.times do
+      channel.receive
+    end
+  end
+
   private def master_install_script(master)
     server = master == first_master ? " --cluster-init " : " --server https://#{api_server_ip_address}:6443 "
     flannel_interface = find_flannel_interface(master)
@@ -112,6 +133,19 @@ class Kubernetes::Installer
       --flannel-iface=#{flannel_interface} \
       #{server} #{tls_sans}" sh -
     SCRIPT
+  end
+
+  private def worker_install_script(worker)
+    flannel_interface = find_flannel_interface(worker)
+
+    <<-BASH
+    curl -sfL https://get.k3s.io | K3S_TOKEN="#{k3s_token}" INSTALL_K3S_VERSION="#{settings.k3s_version}" K3S_URL=https://#{first_master.private_ip_address}:6443 INSTALL_K3S_EXEC="agent \
+      --node-name="$(hostname -f)" \
+      --kubelet-arg="cloud-provider=external" \
+      --node-ip=$(hostname -I | awk '{print $2}') \
+      --node-external-ip=$(hostname -I | awk '{print $1}') \
+      --flannel-iface=#{flannel_interface}" sh -
+    BASH
   end
 
   private def find_flannel_interface(server)
