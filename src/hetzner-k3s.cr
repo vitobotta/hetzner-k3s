db4fb4f596342ece58d0e1e748fe673519f07f3d
Vito Botta <vito@botta.me>
2023-01-02 16:55:07 +0200

    Cache the k3s releases in a temp file

--- src/hetzner-k3s.cr
+++ src/hetzner-k3s.cr
@@ -8,7 +8,7 @@ require "./cluster/upgrade"
 
 module Hetzner::K3s
   class CLI < Admiral::Command
-    VERSION = "0.6.9"
+    VERSION = "0.7.0"
 
     class Create < Admiral::Command
       define_help description: "create - Create a cluster"
