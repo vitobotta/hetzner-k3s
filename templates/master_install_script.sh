6e83bfec17cb602f7cc3aad6db43b12b5023d985
Vito Botta <vito@botta.me>
2023-01-07 22:41:36 +0200

    Allow using an OS image other than Ubuntu; upgrade cluster autoscaler

--- templates/master_install_script.sh
+++ templates/master_install_script.sh
@@ -1,4 +1,9 @@
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_EXEC="server \
+PRIVATE_IP=""
+
+while [ -z "$PRIVATE_IP" ]; do
+  PRIVATE_IP=$(ip -oneline -4 addr show scope global | tr -s ' ' | tr '/' ' ' | cut -f 2,4 -d ' ' | grep $(if lscpu | grep Vendor | grep -q Intel; then echo ens10 ; else echo enp7s0 ; fi) | awk '{print $2}')
+  sleep 1
+done && curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_EXEC="server \
 --disable-cloud-controller \
 --disable servicelb \
 --disable traefik \
@@ -14,8 +19,8 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN
 --kube-scheduler-arg="bind-address=0.0.0.0" \
 {{ taint }} {{ extra_args }} \
 --kubelet-arg="cloud-provider=external" \
---advertise-address=$(hostname -I | awk '{print $2}') \
---node-ip=$(hostname -I | awk '{print $2}') \
+--advertise-address=$PRIVATE_IP \
+--node-ip=$PRIVATE_IP \
 --node-external-ip=$(hostname -I | awk '{print $1}') \
 --flannel-iface="$(if lscpu | grep Vendor | grep -q Intel; then echo ens10 ; else echo enp7s0 ; fi)" \
 {{ server }} {{ tls_sans }}" sh -
--- templates/worker_install_script.sh
+++ templates/worker_install_script.sh
@@ -1,6 +1,11 @@
-curl -sfL https://get.k3s.io | K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_URL=https://{{ first_master_private_ip_address }}:6443 INSTALL_K3S_EXEC="agent \
+PRIVATE_IP=""
+
+while [ -z "$PRIVATE_IP" ]; do
+  PRIVATE_IP=$(ip -oneline -4 addr show scope global | tr -s ' ' | tr '/' ' ' | cut -f 2,4 -d ' ' | grep $(if lscpu | grep Vendor | grep -q Intel; then echo ens10 ; else echo enp7s0 ; fi) | awk '{print $2}')
+  sleep 1
+done && curl -sfL https://get.k3s.io | K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_URL=https://{{ first_master_private_ip_address }}:6443 INSTALL_K3S_EXEC="agent \
 --node-name="$(hostname -f)" \
 --kubelet-arg="cloud-provider=external" \
---node-ip=$(hostname -I | awk '{print $2}') \
+--node-ip=$PRIVATE_IP \
 --node-external-ip=$(hostname -I | awk '{print $1}') \
 --flannel-iface=$(if lscpu | grep Vendor | grep -q Intel; then echo ens10 ; else echo enp7s0 ; fi)" sh -
