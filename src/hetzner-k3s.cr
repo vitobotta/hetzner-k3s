364e994713272123e29079f638dc79a4f1503e18
Vito Botta <vito@botta.me>
2023-01-16 01:07:06 +0200

    Allow old crypto on Redhat systems

--- src/hetzner-k3s.cr
+++ src/hetzner-k3s.cr
@@ -8,7 +8,7 @@ require "./cluster/upgrade"
 
 module Hetzner::K3s
   class CLI < Admiral::Command
-    VERSION = "1.0.6"
+    VERSION = "1.0.7"
 
     class Create < Admiral::Command
       define_help description: "create - Create a cluster"
