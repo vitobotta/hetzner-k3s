30ba31d1b25fe4e60a45edb2ff469a3afb7bfbf8
Vito Botta <vito@botta.me>
2023-04-26 21:07:16 +0300

    Cleanup

--- src/hetzner/server/find.cr
+++ src/hetzner/server/find.cr
@@ -14,10 +14,6 @@ class Hetzner::Server::Find
     servers.find do |server|
       server.name == server_name
     end
-  end
-
-  private def fetch_servers
-    ServersList.from_json(hetzner_client.get("/servers")).servers
   rescue ex : Crest::RequestFailed
     STDERR.puts "Failed to fetch servers: #{ex.message}"
     STDERR.puts ex.response
