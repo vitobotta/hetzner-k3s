02116973d05963d8e4b79d1679b8fd841e56f625
Matthijs Galesloot <matthijs.galesloot@gmail.com>
2023-09-10 09:02:37 +0200

    private IP of LB in san of k8s api

--- src/hetzner/load_balancer.cr
+++ src/hetzner/load_balancer.cr
@@ -6,9 +6,14 @@ class Hetzner::LoadBalancer
 
   property id : Int32
   property name : String
+  property private_net : Array(Hetzner::Ipv4)
   getter public_net : PublicNet?
-
+  
   def public_ip_address
     public_net.try(&.ipv4).try(&.ip)
+  end
+
+  def private_ip_address
+    private_net[0].try(&.ip)
   end
 end
