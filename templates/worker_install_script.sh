fe66ba2f0af76715431dff8e15a8d9c88e961135
Vito Botta <vito@botta.me>
2024-04-18 20:00:57 +0000

    Update worker install script

--- templates/worker_install_script.sh
+++ templates/worker_install_script.sh
@@ -10,22 +10,17 @@ PUBLIC_IP=$(hostname -I | awk '{print $1}')
 
 if [[ "{{ private_network_enabled }}" = "true" ]]; then
   PRIVATE_IP=$(ip route get {{ private_network_test_ip }} | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-  NETWORK_INTERFACE=" --flannel-iface=$(ip route get {{ private_network_test_ip }} | awk -F"dev " 'NR==1{split($2,a," ");print a[1]}')"
+  NETWORK_INTERFACE=" --flannel-iface=$(ip route get {{ private_network_test_ip }} | awk -F"dev " 'NR==1{split($2,a," ");print a[1]}') "
 else
   PRIVATE_IP="${PUBLIC_IP}"
-  NETWORK_INTERFACE=""
-fi
-
-if [[ "{{ cni }}" = "flannel" ]]; then
-  FLANNEL_SETTINGS=" {{ flannel_backend }} $NETWORK_INTERFACE "
-else
-  FLANNEL_SETTINGS=" --flannel-backend=none --disable-kube-proxy --disable-network-policy "
+  NETWORK_INTERFACE=" "
 fi
 
 curl -sfL https://get.k3s.io | K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_URL=https://{{ first_master_private_ip_address }}:6443 INSTALL_K3S_EXEC="agent \
 --node-name=$HOSTNAME {{ extra_args }} \
 --node-ip=$PRIVATE_IP \
---node-external-ip=$PUBLIC_IP $NETWORK_INTERFACE" sh -
+--node-external-ip=$PUBLIC_IP \
+$NETWORK_INTERFACE " sh -
 
 systemctl start k3s-agent # on some OSes the service doesn't start automatically for some reason
 
