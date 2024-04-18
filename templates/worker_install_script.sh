a2a052cbaf6d24636fb7f13ff4b9fb03af20d22b
Vito Botta <vito@botta.me>
2024-04-18 13:00:55 +0000

    Fix DNS issue

--- templates/worker_install_script.sh
+++ templates/worker_install_script.sh
@@ -13,6 +13,7 @@ NETWORK_INTERFACE=$(ip route get {{ private_network_test_ip }} | awk -F"dev " 'N
 curl -sfL https://get.k3s.io | K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_URL=https://{{ first_master_private_ip_address }}:6443 INSTALL_K3S_EXEC="agent \
 --node-name=$HOSTNAME \
 --kubelet-arg="cloud-provider=external" \
+--kubelet-arg="resolv-conf=/etc/k8s-resolv.conf" \
 --node-ip=$PRIVATE_IP \
 --node-external-ip=$PUBLIC_IP \
 --flannel-iface=$NETWORK_INTERFACE" sh -
