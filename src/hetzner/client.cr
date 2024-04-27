d618ae01d61847af8125345d2be09197804786cf
Vito Botta <vito@botta.me>
2024-04-27 20:17:51 +0000

    Refactor Hetzner::Client methods to handle rate limiting and response handling

--- src/hetzner/client.cr
+++ src/hetzner/client.cr
@@ -29,54 +29,98 @@ class Hetzner::Client
   end
 
   def get(path, params : Hash = {} of Symbol => String | Bool | Nil) : String
-    response = Crest.get(
-      "#{api_url}#{path}",
-      params: params,
-      json: true,
-      headers: headers
-    )
+    response = with_rate_limit do
+      Crest.get(
+        "#{api_url}#{path}",
+        params: params,
+        json: true,
+        headers: headers,
+        handle_errors: false
+      )
+    end
 
-    response.body
+    handle_response(response)
   end
 
   def post(path, params = {} of KeyType => ValueType)
-    response = Crest.post(
-      "#{api_url}#{path}",
-      params.to_json,
-      json: true,
-      headers: headers,
-      handle_errors: false
-    )
+    response = with_rate_limit do
+      Crest.post(
+        "#{api_url}#{path}",
+        params.to_json,
+        json: true,
+        headers: headers,
+        handle_errors: false
+      )
+    end
 
-    success = response.status_code >= 200 && response.status_code < 300
-
-    [success, response.body]
+    handle_response(response)
   end
 
   def put(path, params = {} of KeyType => ValueType)
-    response = Crest.put(
-      "#{api_url}#{path}",
-      params.to_json,
-      json: true,
-      headers: headers
-    )
+    response = with_rate_limit do
+      Crest.put(
+        "#{api_url}#{path}",
+        params.to_json,
+        json: true,
+        headers: headers,
+        handle_errors: false
+      )
+    end
 
-    response.body
+    handle_response(response)
   end
 
   def delete(path, id)
-    response = Crest.delete(
-      "#{api_url}#{path}/#{id}",
-      json: true,
-      headers: headers
-    )
+    response = with_rate_limit do
+      Crest.delete(
+        "#{api_url}#{path}/#{id}",
+        json: true,
+        headers: headers,
+        handle_errors: false
+      )
+    end
 
-    response.body
+    handle_response(response)
   end
 
   private def headers
     {
       "Authorization" => "Bearer #{token}",
     }
+  end
+
+  private def handle_rate_limit(response)
+    reset_timestamp = response.headers["ratelimit-reset"]
+
+    return unless reset_timestamp.is_a?(String)
+
+    reset_time = reset_timestamp.to_i
+    wait_time = reset_time - Time.utc.to_unix + 30
+
+    while wait_time > 0
+      reset_time = Time.utc.to_unix + wait_time
+      puts "Hetzner API Rate Limit hit. Waiting until #{Time.unix(reset_time).to_s} for reset..."
+      sleep_time = [wait_time, 5].min
+      sleep(sleep_time)
+      wait_time -= sleep_time
+    end
+  end
+
+  private def with_rate_limit
+    while true
+      response = yield
+
+      if response.status_code == 429
+        handle_rate_limit(response)
+      else
+        return response
+      end
+    end
+  end
+
+  private def handle_response(response)
+    success = response.status_code >= 200 && response.status_code < 300
+
+    [success, response.body]
   end
 end
