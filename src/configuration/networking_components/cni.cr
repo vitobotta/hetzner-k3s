eebdaddd62b079a43d020ff086954c3d6767aa29
Vito Botta <vito@botta.me>
2024-04-28 15:22:44 +0000

    Add CNI mode attributes and helper methods to CNI configuration class

--- src/configuration/networking_components/cni.cr
+++ src/configuration/networking_components/cni.cr
@@ -20,4 +20,20 @@ class Configuration::NetworkingComponents::CNI
       errors << "CNI mode must be either 'flannel' or 'cilium' when CNI is enabled"
     end
   end
+
+  def flannel?
+    enabled? && mode == "flannel"
+  end
+
+  def cilium?
+    enabled && mode == "cilium"
+  end
+
+  def encryption?
+    encryption
+  end
+
+  def enabled?
+    enabled
+  end
 end
