a192b32cfa13c0082d288287bf397b322bcde143
Vito Botta <vito@botta.me>
2024-04-28 00:15:50 +0000

    Reduce API calls by finding existing nodes with kubectl

--- src/hetzner/instance.cr
+++ src/hetzner/instance.cr
@@ -35,4 +35,12 @@ class Hetzner::Instance
   def master?
     /-master\d+/ =~ name
   end
+
+  def initialize(id : Int32, status : String, instance_name : String, internal_ip : String, external_ip : String)
+    @id = id
+    @status = status
+    @name = instance_name
+    @public_net = PublicNet.new(external_ip) unless external_ip.blank?
+    @private_net = [NetworkInterface.new(internal_ip)] unless internal_ip.blank?
+  end
 end
