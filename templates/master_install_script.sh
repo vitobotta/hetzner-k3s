3f11f642f3e6455c803add27acdc0150d4362502
Gunther Klessinger <gunther.klessinger@axiros.com>
2024-07-15 23:21:05 +0200

    Formatting fix (spaces)
    sry, my diff tool failed me

--- templates/master_install_script.sh
+++ templates/master_install_script.sh
@@ -1,13 +1,13 @@
 while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
-	echo "Awaiting cloud boot-finished..."
+	echo "Awaiting cloud/instance/boot-finished..."
 	sleep 5
 done
 
 touch /etc/initialized
 
-if [[ $(</etc/initialized) != "true" ]]; then
-	systemctl restart NetworkManager || true
-	dhclient eth1 -v || true
+if [[ $(< /etc/initialized) != "true" ]]; then
+  systemctl restart NetworkManager || true
+  dhclient eth1 -v || true
 fi
 
 HOSTNAME=$(hostname -f)
@@ -16,9 +16,9 @@ PUBLIC_IP=$(hostname -I | awk '{print $1}')
 NETWORK_INTERFACE=$(ip route get {{ private_network_test_ip }} | awk -F"dev " 'NR==1{split($2,a," ");print a[1]}')
 
 if [[ "{{ disable_flannel }}" = "true" ]]; then
-	FLANNEL_SETTINGS=" --flannel-backend=none --disable-kube-proxy --disable-network-policy "
+  FLANNEL_SETTINGS=" --flannel-backend=none --disable-kube-proxy --disable-network-policy "
 else
-	FLANNEL_SETTINGS=" {{ flannel_backend }} --flannel-iface=$NETWORK_INTERFACE "
+  FLANNEL_SETTINGS=" {{ flannel_backend }} --flannel-iface=$NETWORK_INTERFACE "
 fi
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN="{{ k3s_token }}" {{ datastore_endpoint }} INSTALL_K3S_EXEC="server \
@@ -45,4 +45,4 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN
 
 systemctl start k3s # on some OSes the service doesn't start automatically for some reason
 
-echo true >/etc/initialized
+echo true > /etc/initialized
