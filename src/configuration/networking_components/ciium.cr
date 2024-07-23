958eb308a3f96bd73ad1b8198242c471d1dca60b
Vito Botta <vito@botta.me>
2024-07-23 15:18:20 +0300

    Upgrade Cilium to latest version

--- src/configuration/networking_components/ciium.cr
+++ src/configuration/networking_components/ciium.cr
@@ -2,7 +2,7 @@ class Configuration::NetworkingComponents::Cilium
   include YAML::Serializable
   include YAML::Serializable::Unmapped
 
-  getter chart_version : String = "v1.15.4"
+  getter chart_version : String = "v1.15.7"
 
   def initialize
   end
