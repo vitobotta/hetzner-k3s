05094a48cdd9d8fa1ac38f65214516b9c7c9d418
Gunther Klessinger <gunther.klessinger@axiros.com>
2024-07-30 17:20:36 +0200

    chore: #389 Cloud init wait improvements
    As discussed in  #389.

--- templates/worker_install_script.sh
+++ templates/worker_install_script.sh
@@ -1,7 +1,16 @@
-while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
-	echo "Awaiting cloud/instance/boot-finished..."
-	sleep 5
-done
+fn_cloud="/var/lib/cloud/instance/boot-finished"
+function await_cloud_init() {
+  echo "🕒 Awaiting cloud config (may take a minute...)"
+  while true; do
+    for _ in $(seq 1 10); do
+      test -f $fn_cloud && return
+      sleep 1
+    done
+    echo -n "."
+  done
+}
+test -f $fn_cloud || await_cloud_init
+echo "Cloud init finished: $(cat $fn_cloud)"
 
 touch /etc/initialized
 
