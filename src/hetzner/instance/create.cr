a1e0fffe0221adf7e79395b25b6221d9f1806270
Vito Botta <vito@botta.me>
2024-08-18 14:16:49 +0300

    Attach instance to network if it doesn't happen automatically as expected

--- src/hetzner/instance/create.cr
+++ src/hetzner/instance/create.cr
@@ -102,9 +102,12 @@ class Hetzner::Instance::Create
   private def ensure_instance_is_ready
     ready = false
     powering_on_count = 0
+    attaching_to_network_count = 0
 
     until ready
-      sleep 10 unless instance_existed
+      if !instance_existed && settings.networking.private_network.enabled
+        sleep 10
+      end
 
       instance = instance_finder.run
 
@@ -112,9 +115,17 @@ class Hetzner::Instance::Create
 
       log_line "Instance status: #{instance.status}"
 
-      if instance.status != "running"
+      if instance.status != "running" && settings.networking.private_network.enabled
         powering_on_count += 1
         power_on_instance(instance, powering_on_count)
+        next
+      end
+
+      sleep 5
+
+      if settings.networking.private_network.enabled && !instance.try(&.private_ip_address)
+        attaching_to_network_count += 1
+        attach_instance_to_network(instance, attaching_to_network_count)
         next
       end
 
@@ -129,6 +140,14 @@ class Hetzner::Instance::Create
     log_line "Powering on instance (attempt #{powering_on_count})"
     hetzner_client.post("/servers/#{instance.id}/actions/poweron", {} of String => String)
     log_line "Waiting for instance to be powered on..."
+  end
+
+  private def attach_instance_to_network(instance, attaching_to_network_count)
+    mutex.synchronize do
+      log_line "Attaching instance to network (attempt #{attaching_to_network_count})"
+      hetzner_client.post("/servers/#{instance.id}/actions/attach_to_network", { network: network.not_nil!.id })
+      log_line "Waiting for instance to be attached to the network..."
+    end
   end
 
   private def instance_config
