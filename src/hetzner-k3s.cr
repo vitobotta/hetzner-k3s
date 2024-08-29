d11f688a07ef09b85b1619269f0dda5f15186ef8
Vito Botta <vito@botta.me>
2024-08-29 17:12:42 +0300

    Bump version

--- src/hetzner-k3s.cr
+++ src/hetzner-k3s.cr
@@ -8,7 +8,7 @@ require "./cluster/upgrade"
 
 module Hetzner::K3s
   class CLI < Admiral::Command
-    VERSION = "2.0.5"
+    VERSION = "2.0.6"
 
     class Create < Admiral::Command
       define_help description: "create - Create a cluster"
