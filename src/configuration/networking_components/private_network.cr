81b7d83a200dad043377e4e13d0c813e7ddeeaad
Vito Botta <vito@botta.me>
2025-03-31 20:51:41 +0000

    Add Tailscale support as an alternative private network mode

--- src/configuration/networking_components/private_network.cr
+++ src/configuration/networking_components/private_network.cr
@@ -1,4 +1,5 @@
 require "ipaddress"
+require "./tailscale"
 
 class Configuration::NetworkingComponents::PrivateNetwork
   include YAML::Serializable
@@ -7,12 +8,16 @@ class Configuration::NetworkingComponents::PrivateNetwork
   getter enabled : Bool = true
   getter subnet : String = "10.0.0.0/16"
   getter existing_network_name : String = ""
+  getter mode : String = "hetzner"
+  getter tailscale : Configuration::NetworkingComponents::Tailscale = Configuration::NetworkingComponents::Tailscale.new
 
   def initialize
   end
 
   def validate(errors, hetzner_client)
     validate_existing_network_name(errors, hetzner_client)
+    validate_mode(errors)
+    validate_tailscale(errors) if tailscale?
 
     begin
       IPAddress.new(subnet).network?
@@ -27,5 +32,19 @@ class Configuration::NetworkingComponents::PrivateNetwork
     return if Hetzner::Network::Find.new(hetzner_client, existing_network_name).run
 
     errors << "You have specified that you want to use the existing network named '#{existing_network_name}' but this network doesn't exist"
+  end
+
+  private def validate_mode(errors)
+    unless ["hetzner", "tailscale"].includes?(mode)
+      errors << "private network mode must be either 'hetzner' or 'tailscale'"
+    end
+  end
+
+  private def tailscale?
+    mode == "tailscale"
+  end
+
+  private def validate_tailscale(errors)
+    tailscale.validate(errors)
   end
 end
