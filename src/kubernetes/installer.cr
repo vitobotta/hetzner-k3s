a164eae1b468376917aa9611f90e3e12711ede9b
Vito Botta <vito@botta.me>
2025-04-01 16:14:40 +0300

    Add Tailscale auth key and server URL to Kubernetes installer settings

--- src/kubernetes/installer.cr
+++ src/kubernetes/installer.cr
@@ -203,6 +203,8 @@ class Kubernetes::Installer
       private_network_enabled: settings.networking.private_network.enabled.to_s,
       private_network_mode: settings.networking.private_network.mode,
       private_network_subnet: settings.networking.private_network.enabled ? settings.networking.private_network.subnet : "",
+      tailscale_auth_key: settings.networking.private_network.tailscale.auth_key,
+      tailscale_server_url: settings.networking.private_network.tailscale.server_url,
       cluster_cidr: settings.networking.cluster_cidr,
       service_cidr: settings.networking.service_cidr,
       cluster_dns: settings.networking.cluster_dns,
