f580742bd0cbcba0decbb5e3f92f4f8dcaedcf35
Theo Mathieu <tmathieu.github@fastmail.com>
2025-05-02 13:36:32 +0200

    feat: wip

--- src/kubernetes/software/hetzner/cloud_controller_manager.cr
+++ src/kubernetes/software/hetzner/cloud_controller_manager.cr
@@ -14,6 +14,10 @@ class Kubernetes::Software::Hetzner::CloudControllerManager
   def install
     log_line "Installing Hetzner Cloud Controller Manager..."
 
+    puts "--- Manifest to apply ---"
+    puts manifest
+    puts "-------------------------"
+
     apply_manifest_from_yaml(manifest, "Failed to install Hetzner Cloud Controller Manager")
 
     log_line "Hetzner Cloud Controller Manager installed"
@@ -25,12 +29,19 @@ class Kubernetes::Software::Hetzner::CloudControllerManager
 
   private def manifest
     manifest_url = if settings.networking.private_network.enabled
-      settings.manifests.cloud_controller_manager_manifest_url
-    else
-      settings.manifests.cloud_controller_manager_manifest_url.gsub("-networks", "")
-    end
+                     settings.manifests.cloud_controller_manager_manifest_url
+                   else
+                     settings.manifests.cloud_controller_manager_manifest_url.gsub("-networks", "")
+                   end
 
     manifest = fetch_manifest(manifest_url)
     manifest.gsub(/--cluster-cidr=[^"]+/, "--cluster-cidr=#{settings.networking.cluster_cidr}")
+
+    if settings.responds_to?(:robot_user) && settings.robot_user
+      manifest = manifest.gsub(
+        /(- name: HCLOUD_TOKEN\s+valueFrom:\s+secretKeyRef:\s+key: token\s+name: hcloud)/m,
+        "\\1\n            - name: ROBOT_ENABLED\n              value: \"true\""
+      )
+    end
   end
 end
