0369a10ea81d4dc7b7b8e7fcc971be6c6b610531
Vito Botta <vito@botta.me>
2025-07-27 21:54:25 +0300

    Refactor placement group cleanup and fetch logic

--- src/hetzner/placement_group/all.cr
+++ src/hetzner/placement_group/all.cr
@@ -14,34 +14,27 @@ class Hetzner::PlacementGroup::All
   end
 
   def delete_unused
-    all_placement_groups = fetch_placement_groups
-
-    all_placement_groups.reject! do |placement_group|
-      if placement_group.servers.size == 0 && placement_group.name.starts_with?(settings.cluster_name)
-        Hetzner::PlacementGroup::Delete.new(hetzner_client, placement_group: placement_group ).run
-        true
-      else
-        false
-      end
+    fetch_placement_groups.reject! do |placement_group|
+      next unless placement_group.servers.empty? && placement_group.name.starts_with?(settings.cluster_name)
+      
+      Hetzner::PlacementGroup::Delete.new(hetzner_client, placement_group: placement_group).run
+      true
     end
-
-    all_placement_groups
   end
 
   def delete_all
     fetch_placement_groups.each do |placement_group|
       next unless placement_group.name.starts_with?(settings.cluster_name)
-      Hetzner::PlacementGroup::Delete.new(hetzner_client, placement_group: placement_group ).run
+      Hetzner::PlacementGroup::Delete.new(hetzner_client, placement_group: placement_group).run
     end
   end
 
   private def fetch_placement_groups
     Retriable.retry(max_attempts: 10, backoff: false, base_interval: 5.seconds) do
-      success, response = hetzner_client.get("/placement_groups", { :per_page => 100 })
+      success, response = hetzner_client.get("/placement_groups", {:per_page => 100})
 
       if success
-        groups = PlacementGroupsList.from_json(response).placement_groups
-        groups.sort_by { |placement_group| placement_group.name }
+        PlacementGroupsList.from_json(response).placement_groups.sort_by(&.name)
       else
         STDERR.puts "[#{default_log_prefix}] Failed to fetch placement groups: #{response}"
         STDERR.puts "[#{default_log_prefix}] Retrying to fetch placement groups in 5 seconds..."
