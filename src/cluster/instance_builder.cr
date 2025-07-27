5eca72f32a894c0e6ab0af42340547d42a4c77b0
Vito Botta <vito@botta.me>
2025-07-28 00:41:13 +0300

    Extract classes

---
+++ src/cluster/instance_builder.cr
@@ -0,0 +1,101 @@
+require "../hetzner/instance/create"
+
+class Cluster::InstanceBuilder
+  private getter settings : Configuration::Main
+  private getter hetzner_client : Hetzner::Client
+  private getter mutex : Mutex
+  private getter ssh_key : Hetzner::SSHKey
+  private getter network : Hetzner::Network?
+
+  def initialize(@settings, @hetzner_client, @mutex, @ssh_key, @network)
+  end
+
+  def build_instance_name(instance_type, index, include_instance_type, prefix = "master") : String
+    instance_type_part = include_instance_type ? "#{instance_type}-" : ""
+    "#{settings.cluster_name}-#{instance_type_part}#{prefix}#{index + 1}"
+  end
+
+  def create_master_instance(index : Int32, placement_group : Hetzner::PlacementGroup?, location : String) : Hetzner::Instance::Create
+    masters_pool = settings.masters_pool
+    legacy_instance_type = masters_pool.legacy_instance_type
+    instance_type = masters_pool.instance_type
+
+    legacy_instance_name = build_instance_name(legacy_instance_type, index, true)
+    instance_name = build_instance_name(instance_type, index, settings.include_instance_type_in_instance_name)
+
+    image = masters_pool.image || settings.image
+    additional_packages = masters_pool.additional_packages || settings.additional_packages
+    additional_pre_k3s_commands = masters_pool.additional_pre_k3s_commands || settings.additional_pre_k3s_commands
+    additional_post_k3s_commands = masters_pool.additional_post_k3s_commands || settings.additional_post_k3s_commands
+
+    Hetzner::Instance::Create.new(
+      settings: settings,
+      hetzner_client: hetzner_client,
+      mutex: mutex,
+      legacy_instance_name: legacy_instance_name,
+      instance_name: instance_name,
+      instance_type: instance_type,
+      image: image,
+      ssh_key: ssh_key,
+      network: network,
+      placement_group: placement_group,
+      additional_packages: additional_packages,
+      additional_pre_k3s_commands: additional_pre_k3s_commands,
+      additional_post_k3s_commands: additional_post_k3s_commands,
+      location: location
+    )
+  end
+
+  def create_worker_instance(index : Int32, node_pool, placement_group : Hetzner::PlacementGroup?) : Hetzner::Instance::Create
+    legacy_instance_type = node_pool.legacy_instance_type
+    instance_type = node_pool.instance_type
+
+    legacy_instance_name = build_instance_name(legacy_instance_type, index, true, "pool-#{node_pool.name}-worker")
+    instance_name = build_instance_name(instance_type, index, settings.include_instance_type_in_instance_name, "pool-#{node_pool.name}-worker")
+
+    image = node_pool.image || settings.image
+    additional_packages = node_pool.additional_packages || settings.additional_packages
+    additional_pre_k3s_commands = node_pool.additional_pre_k3s_commands || settings.additional_pre_k3s_commands
+    additional_post_k3s_commands = node_pool.additional_post_k3s_commands || settings.additional_post_k3s_commands
+
+    Hetzner::Instance::Create.new(
+      settings: settings,
+      hetzner_client: hetzner_client,
+      mutex: mutex,
+      legacy_instance_name: legacy_instance_name,
+      instance_name: instance_name,
+      instance_type: instance_type,
+      image: image,
+      location: node_pool.location || default_masters_Location,
+      ssh_key: ssh_key,
+      network: network,
+      placement_group: placement_group,
+      additional_packages: additional_packages,
+      additional_pre_k3s_commands: additional_pre_k3s_commands,
+      additional_post_k3s_commands: additional_post_k3s_commands
+    )
+  end
+
+  def initialize_master_instances(masters_locations, placement_group) : Array(Hetzner::Instance::Create)
+    Array(Hetzner::Instance::Create).new(settings.masters_pool.instance_count) do |i|
+      create_master_instance(i, placement_group, masters_locations[i])
+    end
+  end
+
+  def initialize_worker_instances : Array(Hetzner::Instance::Create)
+    factories = Array(Hetzner::Instance::Create).new
+    static_worker_node_pools = settings.worker_node_pools.reject(&.autoscaling_enabled)
+
+    static_worker_node_pools.each do |node_pool|
+      node_pool.instance_count.times do |i|
+        factories << create_worker_instance(i, node_pool, nil)
+      end
+    end
+
+    factories
+  end
+
+  private def default_masters_Location
+    settings.masters_pool.locations.first
+  end
+end
---
+++ src/cluster/placement_group_manager.cr
@@ -0,0 +1,91 @@
+require "../hetzner/placement_group/create"
+require "../hetzner/placement_group/all"
+
+class Cluster::PlacementGroupManager
+  MAX_PLACEMENT_GROUPS = 50
+  MAX_INSTANCES_PER_PLACEMENT_GROUP = 9
+
+  private getter settings : Configuration::Main
+  private getter hetzner_client : Hetzner::Client
+  private getter mutex : Mutex
+  getter all_placement_groups : Array(Hetzner::PlacementGroup)
+
+  def initialize(@settings, @hetzner_client, @mutex, @all_placement_groups)
+  end
+
+  def find_by_name(placement_group_name)
+    all_placement_groups.find { |pg| pg.name == placement_group_name }
+  end
+
+  def exists?(placement_group_name)
+    find_by_name(placement_group_name)
+  end
+
+  def create_and_track(placement_group_name)
+    Hetzner::PlacementGroup::Create.new(hetzner_client, placement_group_name).run.tap { |pg| track(pg) }
+  end
+
+  def create_for_masters
+    placement_group_name = "#{settings.cluster_name}-masters"
+    find_by_name(placement_group_name) || create_and_track(placement_group_name)
+  end
+
+  def create_for_node_pool(node_pool, remaining_placement_groups, placement_groups_channel)
+    created_placement_groups = 0
+
+    # Calculate exact number of placement groups needed
+    # Hetzner placement groups have a strict limit of 10 instances per group
+    needed_groups = (node_pool.instance_count + MAX_INSTANCES_PER_PLACEMENT_GROUP - 1) // MAX_INSTANCES_PER_PLACEMENT_GROUP
+    placement_groups_count = [needed_groups, remaining_placement_groups].min
+
+    # Get existing placement groups for this pool
+    pool_placement_groups = all_placement_groups.select { |pg| pg.name.includes?("#{settings.cluster_name}-#{node_pool.name}-") }
+
+    # Create missing placement groups starting from the next sequential index
+    current_groups_count = pool_placement_groups.size
+    (1..placement_groups_count).each do |group_number|
+      break if created_placement_groups >= remaining_placement_groups
+
+      placement_group_name = "#{settings.cluster_name}-#{node_pool.name}-#{group_number}"
+      next if exists?(placement_group_name)
+
+      spawn do
+        placement_group = create_and_track(placement_group_name)
+        placement_groups_channel.send(placement_group)
+      end
+
+      created_placement_groups += 1
+    end
+
+    created_placement_groups
+  end
+
+  def create_for_worker_node_pools(node_pools)
+    node_pools = node_pools.sort_by(&.name.not_nil!)
+
+    remaining_placement_groups = MAX_PLACEMENT_GROUPS - all_placement_groups.size
+    placement_groups_channel = Channel(Hetzner::PlacementGroup).new
+    created_placement_groups = 0
+
+    node_pools.each do |node_pool|
+      next if node_pool.instance_count <= 0
+
+      new_groups = create_for_node_pool(node_pool, remaining_placement_groups, placement_groups_channel)
+      created_placement_groups += new_groups
+      remaining_placement_groups -= new_groups
+
+      break if remaining_placement_groups <= 0
+    end
+
+    created_placement_groups.times { placement_groups_channel.receive }
+  end
+
+  def delete_unused
+    mutex.synchronize { @all_placement_groups = Hetzner::PlacementGroup::All.new(settings, hetzner_client).delete_unused }
+  end
+
+  private def track(placement_group)
+    mutex.synchronize { all_placement_groups << placement_group unless exists?(placement_group.name) }
+    placement_group
+  end
+end
