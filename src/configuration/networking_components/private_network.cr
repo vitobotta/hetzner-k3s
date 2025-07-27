977d1d5c4f77774bd838cba77ced1d5a8ea827a7
Vito Botta <vito@botta.me>
2025-07-27 22:35:21 +0300

    Refactor subnet validation into separate method

--- src/configuration/networking_components/private_network.cr
+++ src/configuration/networking_components/private_network.cr
@@ -13,19 +13,19 @@ class Configuration::NetworkingComponents::PrivateNetwork
 
   def validate(errors, hetzner_client)
     validate_existing_network_name(errors, hetzner_client)
+    validate_subnet(errors)
+  end
 
-    begin
-      IPAddress.new(subnet).network?
-    rescue ArgumentError
-      errors << "private network subnet #{subnet} is not a valid network in CIDR notation"
-    end
+  private def validate_subnet(errors)
+    IPAddress.new(subnet).network?
+  rescue ArgumentError
+    errors << "private network subnet #{subnet} is not a valid network in CIDR notation"
   end
 
   private def validate_existing_network_name(errors, hetzner_client)
     return if existing_network_name.empty?
-
     return if Hetzner::Network::Find.new(hetzner_client, existing_network_name).run
-
+    
     errors << "You have specified that you want to use the existing network named '#{existing_network_name}' but this network doesn't exist"
   end
 end
