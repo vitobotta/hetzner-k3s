56ba86f4fa3956d092e64372c7b37fd964aed06f
Vito Botta <vito@botta.me>
2025-07-29 23:58:58 +0300

    Add optional custom Helm values file for Cilium installation

--- src/configuration/networking_components/ciium.cr
+++ src/configuration/networking_components/ciium.cr
@@ -3,6 +3,7 @@ class Configuration::NetworkingComponents::Cilium
   include YAML::Serializable::Unmapped
 
   getter chart_version : String = "v1.17.2"
+  getter helm_values_path : String?
 
   def initialize
   end
--- src/kubernetes/software/cilium.cr
+++ src/kubernetes/software/cilium.cr
@@ -16,31 +16,39 @@ class Kubernetes::Software::Cilium
   def install
     log_line "Installing Cilium..."
 
-    command = <<-BASH
-    helm repo add cilium https://helm.cilium.io/
+    helm_values_path = settings.networking.cni.cilium.helm_values_path
+    
+    helm_repo_command = "helm repo add cilium https://helm.cilium.io/"
+    helm_install_command = "helm upgrade --install --version #{settings.networking.cni.cilium.chart_version} --namespace kube-system"
+    
+    if helm_values_path && File.exists?(helm_values_path)
+      helm_install_command += " --values #{helm_values_path}"
+    else
+      helm_install_command += " --set encryption.enabled=#{settings.networking.cni.encryption.to_s}"
+      helm_install_command += " --set encryption.type=wireguard"
+      helm_install_command += " --set encryption.nodeEncryption=#{settings.networking.cni.encryption.to_s}"
+      helm_install_command += " --set routingMode=tunnel"
+      helm_install_command += " --set tunnelProtocol=vxlan"
+      helm_install_command += " --set ipam.mode=\"kubernetes\""
+      helm_install_command += " --set kubeProxyReplacement=true"
+      helm_install_command += " --set hubble.enabled=true"
+      helm_install_command += " --set hubble.metrics.enabled=\"{dns,drop,tcp,flow,port-distribution,icmp,http}\""
+      helm_install_command += " --set hubble.relay.enabled=true"
+      helm_install_command += " --set hubble.ui.enabled=true"
+      helm_install_command += " --set k8sServiceHost=127.0.0.1"
+      helm_install_command += " --set k8sServicePort=6444"
+      helm_install_command += " --set operator.replicas=1"
+      helm_install_command += " --set operator.resources.requests.memory=128Mi"
+      helm_install_command += " --set resources.requests.memory=512Mi"
+      helm_install_command += " --set egressGateway.enabled=#{settings.networking.cni.cilium_egress_gateway.to_s}"
+      helm_install_command += " --set bpf.masquerade=#{settings.networking.cni.cilium_egress_gateway.to_s}"
+    end
+    
+    helm_install_command += " cilium cilium/cilium"
 
-    helm upgrade --install \
-    --version #{settings.networking.cni.cilium.chart_version} \
-    --namespace kube-system \
-    --set encryption.enabled=#{settings.networking.cni.encryption.to_s} \
-    --set encryption.type=wireguard \
-    --set encryption.nodeEncryption=#{settings.networking.cni.encryption.to_s} \
-    --set routingMode=tunnel \
-    --set tunnelProtocol=vxlan \
-    --set ipam.mode="kubernetes" \
-    --set kubeProxyReplacement=true \
-    --set hubble.enabled=true \
-    --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,http}" \
-    --set hubble.relay.enabled=true \
-    --set hubble.ui.enabled=true \
-    --set k8sServiceHost=127.0.0.1 \
-    --set k8sServicePort=6444 \
-    --set operator.replicas=1 \
-    --set operator.resources.requests.memory=128Mi \
-    --set resources.requests.memory=512Mi \
-    --set egressGateway.enabled=#{settings.networking.cni.cilium_egress_gateway.to_s} \
-    --set bpf.masquerade=#{settings.networking.cni.cilium_egress_gateway.to_s} \
-    cilium cilium/cilium
+    command = <<-BASH
+    #{helm_repo_command}
+    #{helm_install_command}
 
     echo "Waiting for Cilium to be ready..."
     kubectl -n kube-system rollout status ds cilium
