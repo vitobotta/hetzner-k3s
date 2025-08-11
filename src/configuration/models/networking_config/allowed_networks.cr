961ac65a06678eca90aac06509d139ba083b7b96
Sebastien Kaul <45340011+KaulSe@users.noreply.github.com>
2025-08-11 10:07:48 +0200

    custom hetzner firewall rules in allowed_networks

--- src/configuration/models/networking_config/allowed_networks.cr
+++ src/configuration/models/networking_config/allowed_networks.cr
@@ -1,10 +1,12 @@
+require "./firewall_rule"
+
 class Configuration::Models::NetworkingConfig::AllowedNetworks
   include YAML::Serializable
   include YAML::Serializable::Unmapped
 
   getter ssh : Array(String) = ["0.0.0.0/0"]
   getter api : Array(String) = ["0.0.0.0/0"]
-  getter custom_firewall_rules : Array(::Configuration::Models::NetworkingConfig::FirewallRule) = [] of ::Configuration::Models::NetworkingConfig::FirewallRule
+  getter custom_firewall_rules : Array(Configuration::Models::NetworkingConfig::FirewallRule) = [] of Configuration::Models::NetworkingConfig::FirewallRule
 
   def initialize
   end
