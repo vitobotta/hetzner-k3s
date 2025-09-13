979e76cdd3a2d428668f0e53e1dfbbb92f751705
Sebastien Kaul <45340011+KaulSe@users.noreply.github.com>
2025-09-13 15:44:16 +0200

    fix versions

--- src/configuration/models/addons_config/system_upgrade_controller.cr
+++ src/configuration/models/addons_config/system_upgrade_controller.cr
@@ -6,8 +6,8 @@ module Configuration
         include YAML::Serializable::Unmapped
 
         getter enabled : Bool
-        getter deployment_manifest_url : String = "https://github.com/rancher/system-upgrade-controller/releases/download/v0.14.2/system-upgrade-controller.yaml"
-        getter crd_manifest_url : String = "https://github.com/rancher/system-upgrade-controller/releases/download/v0.14.2/crd.yaml"
+        getter deployment_manifest_url : String = "https://github.com/rancher/system-upgrade-controller/releases/download/v0.16.0/system-upgrade-controller.yaml"
+        getter crd_manifest_url : String = "https://github.com/rancher/system-upgrade-controller/releases/download/v0.16.0/crd.yaml"
 
         def initialize(@enabled : Bool = true)
         end
