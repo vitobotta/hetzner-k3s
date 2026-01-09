b3d3b4caceccf7284ce41e5157af0b968f039731
Vito Botta <vito@botta.me>
2026-01-09 11:17:37 +0200

    Bump version to 2.4.5.rc2

--- src/hetzner-k3s.cr
+++ src/hetzner-k3s.cr
@@ -10,7 +10,7 @@ require "./cluster/run"
 
 module Hetzner::K3s
   class CLI < Admiral::Command
-    VERSION = "2.4.5.rc1"
+    VERSION = "2.4.5.rc2"
 
     def self.print_banner
       puts "╭─────────────────────────────────────────╮".colorize(:green)
