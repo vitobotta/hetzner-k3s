8dfefb89333e630355de897d4d85e2be03c67dd7
Vito Botta <vito@botta.me>
2026-04-04 19:13:06 +0300

    Update to version 2.4.7 across all installation instructions and source code

--- src/hetzner-k3s.cr
+++ src/hetzner-k3s.cr
@@ -10,7 +10,7 @@ require "./cluster/run"
 
 module Hetzner::K3s
   class CLI < Admiral::Command
-    VERSION = "2.4.6"
+    VERSION = "2.4.7"
 
     def self.print_banner
       puts "╭─────────────────────────────────────────╮".colorize(:green)
