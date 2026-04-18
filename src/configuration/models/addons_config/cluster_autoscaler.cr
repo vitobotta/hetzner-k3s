6b62fb95971e368b5aafa1b136830b466a1bd9d3
Vito Botta <vito@botta.me>
2026-04-18 16:35:50 +0300

    Downgraded cluster autoscaler

--- src/configuration/models/addons_config/cluster_autoscaler.cr
+++ src/configuration/models/addons_config/cluster_autoscaler.cr
@@ -15,7 +15,7 @@ class Configuration::Models::AddonsConfig::ClusterAutoscaler
 
   # Manifest URL and image tag moved from global manifests block
   property manifest_url : String = "https://raw.githubusercontent.com/kubernetes/autoscaler/refs/heads/master/cluster-autoscaler/cloudprovider/hetzner/examples/cluster-autoscaler-run-on-master.yaml"
-  property container_image_tag : String = "v1.35.0"
+  property container_image_tag : String = "v1.34.3"
 
   def initialize(@enabled : Bool = true)
   end
